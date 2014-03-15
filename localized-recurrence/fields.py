1fbf85678e09124b29e23a35c5964065f6ac194d
Erik Swanson <theerikswanson@gmail.com>
2014-03-15 16:59:37 -0400

    Initial code import.
    This commit includes most of the code from the original closed-source
    repository. It is not entirely functional, but will be made more so.

---
+++ localized-recurrence/fields.py
@@ -0,0 +1,82 @@
+from datetime import timedelta
+import re
+
+from django.db.models import SubfieldBase
+from django.db.models.fields import IntegerField
+
+
+class DurationField(IntegerField):
+    """A field to store durations of time with accuracy to the second.
+
+    Automatically converts between python timedelta objects and
+    database integers.
+
+    The timedeltas are stored as seconds in the backend, so sub-second
+    accuracy is lost with this field.
+    """
+    description = "A duration of time."
+    __metaclass__ = SubfieldBase
+
+    def to_python(self, value):
+        """Convert a stored duration into a python datetime.timedelta object.
+
+        Need to handle three cases:
+            - An instance of the correct type (timedelta).
+            - A string (e.g., from a deserializer).
+            - What the database returns (integer).
+        """
+        if isinstance(value, timedelta):
+            v = value
+        elif isinstance(value, (str, unicode)):
+            # The string should be in the form "[D day[s],][H]H:MM:SS[.UUUUUU]"
+            try:
+                v = parse_timedelta_string(value)
+            except ValueError:
+                raise ValueError("Duration string must be in the form '[D day[s],][H]H:MM:SS[.UUUUUU]'")
+        elif isinstance(value, int):
+            v = timedelta(seconds=value)
+        elif value is None:
+            v = None
+        else:
+            raise ValueError("Not a valid Duration object")
+        return v
+
+    def get_prep_value(self, value):
+        """Convert a timedelta to integer for storage.
+        """
+        # import pdb; pdb.set_trace()
+        value = self.to_python(value)
+        return int(value.total_seconds())
+
+    def value_to_string(self, obj):
+        """Used by serializers to get a string representation.
+        """
+        time_delta_value = self._get_val_from_obj(obj)
+        return time_delta_value.__str__()
+
+
+def parse_timedelta_string(string):
+    """Parses strings from datetime.timedelta.__str__.
+
+    Usefull because django's force_text uses .__str__ instead of
+    .__repr__
+
+    datetime.timedelta.__str__ returns a string in the form [D day[s],
+    ][H]H:MM:SS[.UUUUUU], where D is negative for negative t.
+    """
+    days_re = "(?P<days>-?[0-9]*) days?, (?P<hours>[0-9]+):(?P<minutes>[0-9]+):(?P<seconds>[0-9]+\.?[0-9]*)"
+    no_days_re = "(?P<hours>[0-9]+):(?P<minutes>[0-9]+):(?P<seconds>[0-9]+\.?[0-9]*)"
+    match_days = re.match(days_re, string)
+    match_no_days = re.match(no_days_re, string)
+    if match_days:
+        return timedelta(**{k: float(v) for k, v in match_days.groupdict().items()})
+    elif match_no_days:
+        return timedelta(**{k: float(v) for k, v in match_no_days.groupdict().items()})
+    else:
+        raise ValueError("'%s' is not in the form [D day[s],][H]H:MM:SS[.UUUUUU]" % string)
+
+try:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ["^ambition\.utils\.duration_field\.DurationField"])
+except ImportError:
+    pass
---
+++ localized-recurrence/tests/models_tests.py
@@ -0,0 +1,258 @@
+from datetime import datetime, timedelta
+
+import pytz
+
+from ambition.utils.test import AmbitionTestCase
+from ..models import LocalizedRecurrence, LocalizedRecurrenceQuerySet, replace_with_offset
+
+
+class Test_LocalizedRecurrenceQuerySet(AmbitionTestCase):
+    """Simple test to ensure the custom query set is being used.
+    """
+    def setUp(self):
+        self.lr_day = LocalizedRecurrence.objects.create(
+            interval='DAY',
+            offset=timedelta(hours=12),
+            timezone=pytz.timezone('US/Eastern'),
+        )
+
+    def test_isinstance(self):
+        recurrences = LocalizedRecurrence.objects.all()
+        self.assertIsInstance(recurrences, LocalizedRecurrenceQuerySet)
+
+
+class Test_LocalizedRecurrenceQuerySet_update_schedule(AmbitionTestCase):
+    """Test that updates to recurrences are reflected in the DB.
+    """
+    def setUp(self):
+        self.lr_day = LocalizedRecurrence.objects.create(
+            interval='DAY',
+            offset=timedelta(hours=12),
+            timezone=pytz.timezone('US/Eastern'),
+        )
+        self.lr_month = LocalizedRecurrence.objects.create(
+            interval='MONTH',
+            offset=timedelta(hours=15),
+            timezone=pytz.timezone('US/Eastern'),
+        )
+
+    def test_update_from_1970(self):
+        """Start with next_scheduled of 1970, after update should be new.
+        """
+        time = datetime(year=2013, month=5, day=20, hour=12, minute=3)
+        LocalizedRecurrence.objects.filter(interval='DAY').update_schedule(time=time)
+        lr_day = LocalizedRecurrence.objects.filter(interval='DAY').first()
+        self.assertGreater(lr_day.next_scheduled, time)
+
+    def test_update_all(self):
+        """Calls to the model manager to update should be passed through.
+        """
+        time = datetime(year=2013, month=5, day=20, hour=15, minute=3)
+        LocalizedRecurrence.objects.update_schedule(time=time)
+        self.assertTrue(all(r.next_scheduled > time for r in LocalizedRecurrence.objects.all()))
+
+
+class Test_LocalizedRecurrence(AmbitionTestCase):
+    """Test the creation and querying of LocalizedRecurrence records.
+    """
+    def setUp(self):
+        self.lr = LocalizedRecurrence.objects.create(
+            interval='DAY',
+            offset=timedelta(hours=12),
+            timezone=pytz.timezone('US/Eastern'),
+        )
+
+    def test_creation_works(self):
+        """Create a single record and get it back in a queryset.
+
+        The record is created in setUp, we just test the return here.
+        """
+        lr_count = LocalizedRecurrence.objects.all().count()
+        self.assertEqual(lr_count, 1)
+
+    def test_timedelta_returned(self):
+        """Test that the Duration field is correctly returning timedeltas.
+        """
+        lr = LocalizedRecurrence.objects.first()
+        self.assertTrue(isinstance(lr.offset, timedelta))
+
+
+class Test_LocalizedRecurrence_utc_of_next_schedule(AmbitionTestCase):
+    def setUp(self):
+        self.lr_day = LocalizedRecurrence.objects.create(
+            interval='DAY',
+            offset=timedelta(hours=12),
+            timezone=pytz.timezone('US/Eastern'),
+        )
+
+        self.lr_week = LocalizedRecurrence.objects.create(
+            interval='WEEK',
+            offset=timedelta(days=3, hours=17, minutes=30),
+            timezone=pytz.timezone('US/Central')
+        )
+
+        self.lr_month = LocalizedRecurrence.objects.create(
+            interval='MONTH',
+            offset=timedelta(days=21, hours=19, minutes=15, seconds=10),
+            timezone=pytz.timezone('US/Central')
+        )
+
+    def test_basic_works(self):
+        """Test a simple case of utc_of_next_schedule.
+
+        - The given recurrence is scheduled daily for Eastern Time at noon.
+        - The given current date in UTC is 2013/1/15::17:05:22
+        - We then expect the next schedule in UTC to be 2013/1/16::17:0:0
+        """
+        current_time = datetime(2013, 1, 15, 17, 5, 22)
+        expected_next_schedule = datetime(2013, 1, 16, 17)
+        schedule_out = self.lr_day.utc_of_next_schedule(current_time)
+        self.assertEqual(schedule_out, expected_next_schedule)
+
+    def test_before_midnight(self):
+        """The case when the scheduled and current time cross midnight.
+
+        - The given recurrence is scheduled daily for Eastern Time at 11:59PM.
+        - The given current date in UTC is 2013/1/15::05:05:22 -> 12:05AM EST
+        - We then expect the next schedule in UTC to be 2013/1/16::05:0:0
+        """
+        self.lr_day.offset = timedelta(hours=23, minutes=59)
+        self.lr_day.save()
+        current_time = datetime(2013, 1, 15, 5, 5, 22)
+        expected_next_schedule = datetime(2013, 1, 16, 4, 59)
+        schedule_out = self.lr_day.utc_of_next_schedule(current_time)
+        self.assertEqual(schedule_out, expected_next_schedule)
+
+    def test_after_midnight(self):
+        """The case when the scheduled and current time are after midnight.
+
+        - The given recurrence is scheduled daily for Eastern Time at 12:01 AM.
+        - The given current date in UTC is 2013/1/15::05:05:22 -> 12:05AM EST
+        - We then expect the next schedule in UTC to be 2013/1/16::05:01:0
+        """
+        self.lr_day.offset = timedelta(minutes=1)
+        self.lr_day.save()
+        current_time = datetime(2013, 1, 15, 5, 5, 22)
+        expected_next_schedule = datetime(2013, 1, 16, 5, 01)
+        schedule_out = self.lr_day.utc_of_next_schedule(current_time)
+        self.assertEqual(schedule_out, expected_next_schedule)
+
+    def test_week_update(self):
+        """Weekly Recurrences should work as expected.
+
+        - Friday August 8th at 10:34 PM UTC is 5:34 PM CDT.
+        - Scheduled for weekly, Friday at 5:30
+        - Expect next schedule to be Friday August 15th at 10:30 PM UTC
+        """
+        current_time = datetime(2013, 8, 8, 22, 34)
+        expected_next_schedule = datetime(2013, 8, 15, 22, 30)
+        schedule_out = self.lr_week.utc_of_next_schedule(current_time)
+        self.assertEqual(schedule_out, expected_next_schedule)
+
+    def test_month_update(self):
+        """Monthly Recurrences should work as expected.
+
+        - Friday August 23rd at 12:34 AM UTC is Friday August 22nd at 7:34 PM CDT.
+        - Scheduled for Monthly, on the 8th day at 7:15.10 PM CDT
+        - Expect next schedule to be September 9 at 12:15.10 AM UTC
+        """
+        current_time = datetime(2013, 8, 23, 0, 34, 55)
+        expected_next_schedule = datetime(2013, 9, 23, 0, 15, 10)
+        schedule_out = self.lr_month.utc_of_next_schedule(current_time)
+        self.assertEqual(schedule_out, expected_next_schedule)
+
+    def test_into_dst_boundary(self):
+        """Recurrences happen at the correct local time after going into DST.
+
+        Going into daylight savings time should mean the UTC time is
+        an hour less on the next recurrence.
+
+        - 2013 US DST began: Sunday March 10th.
+        - Weekly recurence, every Thursday at Noon EST.
+        """
+        self.lr_week.offset = timedelta(days=3, hours=12)
+        self.lr_week.save()
+        current_time = datetime(2013, 3, 7, 18)
+        expected_next_schedule = datetime(2013, 3, 14, 17)
+        schedule_out = self.lr_week.utc_of_next_schedule(current_time)
+        self.assertEqual(schedule_out, expected_next_schedule)
+
+    def test_out_of_dst_boundary(self):
+        """Recurrences at the correct local time after going out of UTC.
+
+        Going into daylight savings time should mean the UTC time is
+        an hour greater on the next recurrence.
+
+        - 2013 US DST ended: Sunday November 3rd.
+        - Weekly recurence, every Thursday at Noon EST.
+        """
+        self.lr_week.offset = timedelta(days=3, hours=12)
+        self.lr_week.save()
+        current_time = datetime(2013, 10, 31, 17)
+        expected_next_schedule = datetime(2013, 11, 7, 18)
+        schedule_out = self.lr_week.utc_of_next_schedule(current_time)
+        self.assertEqual(schedule_out, expected_next_schedule)
+
+    def test_utc_plus(self):
+        """Test a timezone that is UTC + 2.
+
+        - Europe/Berlin DST is UTC + 2
+        """
+        self.lr_day.timezone = pytz.timezone('Europe/Berlin')
+        self.lr_day.save()
+        current_time = datetime(2013, 5, 5, 10, 10)
+        expected_next_schedule = datetime(2013, 5, 6, 10)
+        schedule_out = self.lr_day.utc_of_next_schedule(current_time)
+        self.assertEqual(schedule_out, expected_next_schedule)
+
+
+class Test_replace_with_offset(AmbitionTestCase):
+    def test_day(self):
+        """replace_with_offset works as expected with a 'DAY' interval.
+        """
+        dt_in = datetime(2013, 1, 20, 12, 45, 48)
+        td_in = timedelta(hours=3, minutes=3, seconds=3)
+        interval_in = 'DAY'
+        dt_expected = datetime(2013, 1, 20, 3, 3, 3)
+        dt_out = replace_with_offset(dt_in, td_in, interval_in)
+        self.assertEqual(dt_out, dt_expected)
+
+    def test_week(self):
+        """replace_with_offset works as expected with a 'WEEK' interval.
+        """
+        dt_in = datetime(2013, 1, 20, 12, 45, 48)
+        td_in = timedelta(days=4, hours=3, minutes=3, seconds=3)
+        interval_in = 'WEEK'
+        dt_expected = datetime(2013, 1, 18, 3, 3, 3)
+        dt_out = replace_with_offset(dt_in, td_in, interval_in)
+        self.assertEqual(dt_out, dt_expected)
+
+    def test_week_on_month_boundary(self):
+        """replace_with_offset using interval 'WEEK' should roll over months
+        correctly.
+        """
+        dt_in = datetime(2013, 7, 30, 12, 45, 48)
+        td_in = timedelta(days=4, hours=3, minutes=3, seconds=3)
+        interval_in = 'WEEK'
+        dt_expected = datetime(2013, 8, 2, 3, 3, 3)
+        dt_out = replace_with_offset(dt_in, td_in, interval_in)
+        self.assertEqual(dt_out, dt_expected)
+
+    def test_month(self):
+        """replace_with_offset works as expected with a 'MONTH' interval.
+        """
+        dt_in = datetime(2013, 1, 20, 12, 45, 48)
+        td_in = timedelta(days=15, hours=3, minutes=3, seconds=3)
+        interval_in = 'MONTH'
+        dt_expected = datetime(2013, 1, 16, 3, 3, 3)
+        dt_out = replace_with_offset(dt_in, td_in, interval_in)
+        self.assertEqual(dt_out, dt_expected)
+
+    def test_bad_interval(self):
+        """A missformed interval should raise a value error
+        """
+        dt_in = datetime(2013, 1, 20, 12, 45, 48)
+        td_in = timedelta(days=15, hours=3, minutes=3, seconds=3)
+        interval_in = 'blah'
+        with self.assertRaises(ValueError):
+            replace_with_offset(dt_in, td_in, interval_in)
