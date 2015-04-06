511fff41160e065d9b48357044cf5265982ad13b
Travis Truett <travis.truett@gmail.com>
2015-04-06 15:10:19 -0400

    Admin Test

--- localized_recurrence/tests/admin_tests.py
+++ localized_recurrence/tests/admin_tests.py
@@ -1,13 +1,26 @@
-from django.contrib.admin.options import ModelAdmin
 from django.contrib.admin.sites import AdminSite
 from django.test import TestCase
 
+from localized_recurrence.admin import LocalizedRecurrenceAdmin
 from localized_recurrence.models import LocalizedRecurrence
 
 
 class LocalizedRecurrenceAdminTest(TestCase):
     """Verify that the admin can load.
     """
-    def test_admin(self):
-        site = AdminSite()
-        ModelAdmin(LocalizedRecurrence, site)
+    def setUp(self):
+        super(LocalizedRecurrenceAdminTest, self).setUp()
+        self.site = AdminSite()
+
+    def test_model_admin_load(self):
+        lr_admin = LocalizedRecurrenceAdmin(LocalizedRecurrence, self.site)
+        self.assertEqual(
+            lr_admin.list_display,
+            (
+                'id',
+                'interval',
+                'timezone',
+                'previous_scheduled',
+                'next_scheduled'
+            )
+        )
