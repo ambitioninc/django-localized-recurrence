dbf1d2094ba5b21bbbc66c1a2a16fc616de34994
Travis Truett <travis.truett@gmail.com>
2015-04-06 14:50:17 -0400

    Admin test tweak.

--- localized_recurrence/tests/admin_tests.py
+++ localized_recurrence/tests/admin_tests.py
@@ -1,8 +1,5 @@
-from django.test import TestCase
-
 from localized_recurrence.admin import LocalizedRecurrenceAdmin
 
 
-class LocalizedRecurrenceAdminTests(TestCase):
-    def test_admin():
-        LocalizedRecurrenceAdmin()
+def test_admin():
+    LocalizedRecurrenceAdmin()
