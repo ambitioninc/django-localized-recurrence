5922c8fb1a9f693e37466dfc374a4b3cdb459675
Wes Okes <wes.okes@gmail.com>
2017-09-19 16:58:41 -0400

    pragma

--- localized_recurrence/fields.py
+++ localized_recurrence/fields.py
@@ -19,12 +19,12 @@ class CastOnAssignDescriptor(object):
         self.field = field
 
     def __get__(self, obj, type=None):
-        if obj is None:
+        if obj is None:  # pragma: no cover
             return self
         return obj.__dict__[self.field.name]
 
     def __set__(self, obj, value):
-        obj.__dict__[self.field.name] = self.field.to_python(value)
+        obj.__dict__[self.field.name] = self.field.to_python(value)  # pragma: no cover
 
 
 class DurationField(IntegerField):
