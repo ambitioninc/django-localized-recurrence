6fcf1651c02a658bdee357381e0c633a019fa170
Wes Okes <wes.okes@gmail.com>
2017-09-19 16:32:21 -0400

    update timezone field

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     ],
     install_requires=[
         'Django>=1.9',
-        'django-timezone-field<2.0',
+        'django-timezone-field>=2.0',
         'fleming>=0.4.6',
         'python-dateutil',
         'pytz',
